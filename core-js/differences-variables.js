77cbc4cc3ce8155e14bbb6db4cde2829e2d9fa14
tearzday <biverdaniil@gmail.com>
2025-12-17 15:46:09 +0300

    feat: add differences-variables

---
+++ core-js/differences-variables.js
@@ -0,0 +1,86 @@
+/* ------------------------------ */
+
+// const name = 'Вася';
+// name = 'Петя';
+
+// console.log(name); // Error or 'Петя' or 'Вася'?
+
+/* ------------------------------ */
+
+/* ------------------------------ */
+
+// let name = 'Вася';
+// name = 'Петя';
+
+// console.log(name); // Error or 'Петя' or 'Вася'?
+
+/* ------------------------------ */
+
+
+/* ------------------------------ */
+
+// console.log(firstPeople); // ?
+// console.log(secondPeople); // ?
+// console.log(thirdPeople); // ?
+
+// var firstPeople = 'Вася';
+// const secondPeople = 'Петя';
+// let thirdPeople = 'Маша';
+
+/* ------------------------------ */
+
+
+// /* ------------------------------ */
+
+// var name = 'Вася';
+// var name = 'Петя';
+
+// console.log(name); // ?
+
+// /* ------------------------------ */
+
+/* ------------------------------ */
+
+// let letFirstPeople = 'Вася';
+// let letFirstPeople = 'Петя';
+
+// console.log(letFirstPeople); // ?
+
+// const constFirstPeople = 'Вася';
+// const constFirstPeople = 'Петя';
+
+// console.log(constFirstPeople, constSecondPeople); // ?
+
+/* ------------------------------ */
+
+
+
+// /* ------------------------------ */
+
+// const name;
+// name = 'Вася';
+
+// console.log(name); // ?
+
+// /* ------------------------------ */
+
+// /* ------------------------------ */
+
+
+// function getValue() {
+//   let letVariable = 'letFirst';
+//   const constVariable = 'constFirst';
+//   var varVariable = 'varFirst';
+
+//     if (true) {
+//         let letVariable = 'letSecond';
+//         const constVariable = 'letSecond';
+//         var varVariable = 'varSecond';
+//     }
+
+//   return `${letVariable} + ${constVariable} + ${varVariable}`;
+// }
+
+// console.log(getValue()); // ?
+
+// /* ------------------------------ */
