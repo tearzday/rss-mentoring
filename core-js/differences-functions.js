8be6f9ee3eceb42c77f34896d3eee571dd3e8453
tearzday <biverdaniil@gmail.com>
2025-12-17 16:14:26 +0300

    feat: add differenes-functions

---
+++ core-js/differences-functions.js
@@ -0,0 +1,66 @@
+/* ------------------------------ */
+
+// function sayInfo() {
+//   return arguments;
+// }
+
+// console.log(sayInfo(1, 2, 3));
+/* ------------------------------ */
+
+/* ------------------------------ */
+
+// const sayInfo1 = function() {
+//   return arguments;
+// }
+
+// const sayInfo2 = () => arguments;
+
+// console.log(sayInfo1(1, 2, 3));
+// console.log(sayInfo2(1, 2, 3));
+
+/* ------------------------------ */
+
+
+/* ------------------------------ */
+
+// const obj1 = {
+//     name: 'Вася',
+//     getName: function() {
+//         return this.name;
+//     }
+// }
+
+// const obj2 = {
+//     name: 'Вася',
+//     getName: () => {
+//         return this.name;
+//     }
+// }
+
+// console.log(obj1.getName());
+// console.log(obj2.getName());
+
+/* ------------------------------ */
+
+/* ------------------------------ */
+
+
+// function firstFn() {}
+// console.log(firstFn.prototype);
+
+// const secondFn = () => {};
+// console.log(secondFn.prototype);
+
+/* ------------------------------ */
+
+/* ------------------------------ */
+// function Person(name) { this.name = name; }
+// const p = new Person("Вася");
+
+// console.log(p.name); // ?
+
+// const Person2 = (name) => { this.name = name; }
+// const p2 = new Person2("Bob");
+
+// console.log(p2.name); // ?
+/* ------------------------------ */
