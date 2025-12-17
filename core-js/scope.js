be1954907e002f47b3b7c103ff8d79f5056b8f50
tearzday <biverdaniil@gmail.com>
2025-12-17 16:49:02 +0300

    feat: add scope

---
+++ core-js/scope.js
@@ -0,0 +1,42 @@
+/* ------------------------------ */
+
+// var globalVar = 'var';
+// let globalLet = 'let';
+// const globalConst = 'const';
+
+// function test() {
+//   console.log(globalVar);   // ?
+//   console.log(globalLet);   // ?
+//   console.log(globalConst); // ?
+// }
+
+// test();
+
+/* ------------------------------ */
+
+/* ------------------------------ */
+
+// function test() {
+//   var x = 10;
+//   let y = 20;
+//   const z = 30;
+//   console.log(x, y, z); // ?
+// }
+
+// test();
+
+// console.log(x, y, z); // ?
+/* ------------------------------ */
+
+
+/* ------------------------------ */
+// if (true) {
+//   var aVar = 'var';
+//   let aLet = 'let';
+//   const aConst = 'const';
+// }
+
+// console.log(aVar);   // ?
+// console.log(aLet);   // ?
+// console.log(aConst); // ?
+/* ------------------------------ */
