2dfb6073ccf99f961a71622ee485ce6236efb7b5
tearzday <biverdaniil@gmail.com>
2025-12-22 18:11:15 +0300

    feat: add context

---
+++ core-js/context.js
@@ -0,0 +1,39 @@
+/* ------------------------------ */
+
+// const user = {
+//     name: 'Alice',
+//     getName: function() {
+//         return this.name;
+//     }
+// }
+
+// console.log(user.getName()); // ?
+
+/* ------------------------------ */
+
+
+// const user = {
+//     name: 'Alice',
+//     getName: function() {
+//         return this.name;
+//     }
+// }
+
+// const fn = user.getName;
+
+// console.log(fn()); // ?
+
+/* ------------------------------ */
+
+// const user = {
+//     name: 'Alice',
+//     getName: function() {
+//         return this.name;
+//     }
+// }
+
+// const fn = function() {
+//   return this.name;
+// };
+
+// console.log(fn()); // ?
